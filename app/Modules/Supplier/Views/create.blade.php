8f2445c0e2e3cfee71bdef92dfc926a67195afc7
taryono <taryono@iglobal.co.id>
2022-11-19 00:53:47 +0700

    update

--- app/Modules/Supplier/Views/create.blade.php
+++ app/Modules/Supplier/Views/create.blade.php
@@ -4,7 +4,7 @@
          {!! text_div('name', ['class' => 'form-control', 'id' => 'name', 'placeholder' => 'Nama', 'required']) !!}
          {!! text_div('phone', ['class' => 'form-control', 'id' => 'phone', 'placeholder' => 'No. Telephone', 'required']) !!}
          {!! text_div('cellphone', ['class' => 'form-control', 'id' => 'cellphone', 'placeholder' => 'No. Handphone', 'required']) !!}
-         {!! textarea_div('address', ['class' => 'form-control', 'id' => 'address', 'placeholder' => 'Alamat', 'required']) !!}                  
+         {!! textarea_div('address', ['class' => 'form-control', 'id' => 'address', 'placeholder' => 'Alamat', 'required', 'rows'=> '3']) !!}                  
      </div>
  </div>
  {{ Form::close() }}
